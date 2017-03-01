650872dd726e1afaa1fdb4500b969303019481f7
Hunter Harrison <h.scott.harrison@tcu.edu>
2017-03-01 01:24:01 +0000

    Added Create Action to save to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170228113551) do
+ActiveRecord::Schema.define(version: 20160808235242) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
